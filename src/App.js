2910b751b9748c7312f879e98ca46837d50906b3
tajikwizard <abdulazizcodeshere@gmail.com>
2024-02-07 09:57:33 +0300

    tailwind css has been added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center h-screen justify-center">
+      <div className='flex flex-col items-center shadow-2xl p-8 rounded-lg'>
+       <h1 className="text-3xl font-bold underline">
+           Abdusamadzoda Abdulazizi Abdurasul
+        </h1>
+        <br/>
+        <p className='text-gray-500 dark:text-gray-400 text-lg underline'>Portfolio Page Cooming Soon!</p>
+        </div>
     </div>
   );
 }
